68428fa60b807ccc6185a4a93727ac5c8fa33c1c
a129924 <a129924@gmail.com>
2025-02-03 12:18:10 +0800

    feat: Add ORM utility methods for engine and session creation
    - Implement ORMUtils class with methods to create sync and async SQLAlchemy engines
    - Add methods to generate sync and async database sessions
    - Support configuration-based engine creation using ORMUrlBaseConfig

---
+++ src/fizzify/utils/orm.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import Session as SqlAlchemySession
+from sqlalchemy.orm import sessionmaker
+
+from ..orm.config import ORMUrlBaseConfig
+
+
+class ORMUtils:
+    @classmethod
+    def get_engine(cls, config: ORMUrlBaseConfig) -> Engine:
+        return create_engine(config.generate_url())
+
+    @classmethod
+    def get_async_engine(cls, config: ORMUrlBaseConfig) -> AsyncEngine:
+        return create_async_engine(config.generate_url())
+
+    @classmethod
+    def get_sync_session(cls, engine: Engine) -> sessionmaker[SqlAlchemySession]:
+        return sessionmaker(bind=engine)
+
+    @classmethod
+    def get_async_session(cls, engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+        return async_sessionmaker(bind=engine, class_=AsyncSession)
