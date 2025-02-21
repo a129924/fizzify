548555df8939bd36132a3acdbfffa054b9ff84e1
andrew <a129924@gmail.com>
2025-02-21 17:37:31 +0800

    test: Add unique constraint and insert_or_ignore test for ORM models
    - Introduce UniqueUser model with unique name constraint
    - Add test for insert_or_ignore method with unique constraint
    - Verify driver name retrieval using ORMUtils
    - Demonstrate unique constraint behavior in SQLite database

--- tests/orm/test_orm.py
+++ tests/orm/test_orm.py
@@ -1,17 +1,26 @@
 from datetime import datetime
 
 from pytest import fixture
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.fizzify.orm.config import ORMEngineSqliteConfig, ORMSqliteConfig
 from src.fizzify.orm.models.sync import SyncBase
 from src.fizzify.orm.session.sync import SyncSessionManager
+from src.fizzify.utils.orm import ORMUtils
 
 
 class User(SyncBase):
     name: Mapped[str]
     age: Mapped[int]
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+
+class UniqueUser(SyncBase):
+    name: Mapped[str]
+    age: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    __table_args__ = (UniqueConstraint("name"),)
 
 
 @fixture
@@ -32,6 +41,10 @@ def sync_session_manager(
     sqlite_config: ORMSqliteConfig, sqlite_engine_config: ORMEngineSqliteConfig
 ) -> SyncSessionManager:
     return SyncSessionManager(config=sqlite_config, engine_config=sqlite_engine_config)
+
+
+def test_sql_name_is_sqlite(sync_session_manager: SyncSessionManager):
+    assert ORMUtils.get_driver_name(sync_session_manager.engine) == "sqlite"
 
 
 def test_sqlite_config(sqlite_config: ORMSqliteConfig):
@@ -105,3 +118,24 @@ def test_sync_delete(sync_session_manager: SyncSessionManager):
         User.delete_one(session, filters=[User.name == "John"])
 
         assert User.find_one(session, filters=[User.name == "John"]) is None
+
+
+def test_unique_user(sync_session_manager: SyncSessionManager):
+    user = UniqueUser(name="John", age=20, created_at=datetime.now())
+
+    with sync_session_manager.get_session() as session:
+        user.__create_table__(sync_session_manager.engine)
+        user.save(session)
+
+        user2 = UniqueUser(name="Andrew", age=21, created_at=datetime.now())
+
+        user2.insert_or_ignore(session)
+
+        assert UniqueUser.find_one(session, filters=[UniqueUser.name == "John"]) == user
+
+        filter_user2 = UniqueUser.find_one(
+            session, filters=[UniqueUser.name == "Andrew"]
+        )
+        assert filter_user2 is not None
+        assert filter_user2.name == "Andrew"
+        assert filter_user2.age == 21
