f5222a7d8f1ac7cc55d645f03e5c7d0b7aedf1c6
andrew <a129924@gmail.com>
2025-03-31 17:28:07 +0800

    refactor: Rename User and UniqueUser classes to FromSqliteUser and FromSqliteUniqueUser
    - Update class names in test_sqlite_orm.py to reflect their origin from SQLite.
    - Adjust all references in test cases to use the new class names for consistency.
    - Ensure that table name assertions and CRUD operations are aligned with the renamed classes.

--- tests/orm/test_sqlite_orm.py
+++ tests/orm/test_sqlite_orm.py
@@ -10,13 +10,13 @@ from src.fizzify.orm.session.sync import SyncSessionManager
 from src.fizzify.utils.orm import ORMUtils
 
 
-class User(SyncBase):
+class FromSqliteUser(SyncBase):
     name: Mapped[str] = mapped_column(primary_key=True)
     age: Mapped[int]
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
 
-class UniqueUser(SyncBase):
+class FromSqliteUniqueUser(SyncBase):
     name: Mapped[str] = mapped_column(primary_key=True)
     age: Mapped[int]
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
@@ -43,8 +43,8 @@ def sync_session_manager(
 
 
 def test_table_name_is_converted_to_lower(sync_session_manager: SyncSessionManager):
-    assert User.__tablename__ == "user"
-    assert UniqueUser.__tablename__ == "unique_user"
+    assert FromSqliteUser.__tablename__ == "from_sqlite_user"
+    assert FromSqliteUniqueUser.__tablename__ == "from_sqlite_unique_user"
 
 
 def test_sql_name_is_sqlite(sync_session_manager: SyncSessionManager):
@@ -63,7 +63,7 @@ def test_sync_session(sync_session_manager: SyncSessionManager):
 
 
 def test_sync_add(sync_session_manager: SyncSessionManager):
-    user = User(name="John", age=20, created_at=datetime.now())
+    user = FromSqliteUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         user.__create_table__(sync_session_manager.engine)
@@ -71,23 +71,25 @@ def test_sync_add(sync_session_manager: SyncSessionManager):
 
 
 def test_sync_find(sync_session_manager: SyncSessionManager):
-    user = User(name="John", age=20, created_at=datetime.now())
+    user = FromSqliteUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         user.__create_table__(sync_session_manager.engine)
         user.save(session)
 
-        found_user = User.find_one(session, filters=[User.name == "John"])
+        found_user = FromSqliteUser.find_one(
+            session, filters=[FromSqliteUser.name == "John"]
+        )
 
         assert found_user == user
 
 
 def test_sync_find_all(sync_session_manager: SyncSessionManager):
     users = [
-        User(name="John", age=20, created_at=datetime.now()),
-        User(name="Jane", age=21, created_at=datetime.now()),
-        User(name="Doe", age=22, created_at=datetime.now()),
-        User(name="Alice", age=23, created_at=datetime.now()),
+        FromSqliteUser(name="John", age=20, created_at=datetime.now()),
+        FromSqliteUser(name="Jane", age=21, created_at=datetime.now()),
+        FromSqliteUser(name="Doe", age=22, created_at=datetime.now()),
+        FromSqliteUser(name="Alice", age=23, created_at=datetime.now()),
     ]
 
     with sync_session_manager.get_session() as session:
@@ -95,62 +97,78 @@ def test_sync_find_all(sync_session_manager: SyncSessionManager):
         for user in users:
             user.save(session)
 
-        found_users = User.find_all(session, filters=[User.name == "John"])
+        found_users = FromSqliteUser.find_all(
+            session, filters=[FromSqliteUser.name == "John"]
+        )
 
         assert found_users[0] == users[0]
 
 
 def test_sync_update(sync_session_manager: SyncSessionManager):
-    user = User(name="John", age=20, created_at=datetime.now())
+    user = FromSqliteUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         user.__create_table__(sync_session_manager.engine)
         user.save(session)
 
-        user.update(session, filters=[User.name == "John"], values={"name": "Jane"})
-        updated_user = User.find_one(session, filters=[User.name == "Jane"])
+        user.update(
+            session, filters=[FromSqliteUser.name == "John"], values={"name": "Jane"}
+        )
+        updated_user = FromSqliteUser.find_one(
+            session, filters=[FromSqliteUser.name == "Jane"]
+        )
 
         assert updated_user is not None
         assert updated_user.name == "Jane"
 
 
 def test_sync_delete(sync_session_manager: SyncSessionManager):
-    user = User(name="John", age=20, created_at=datetime.now())
+    user = FromSqliteUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         user.__create_table__(sync_session_manager.engine)
         user.save(session)
 
-        User.delete_one(session, filters=[User.name == "John"])
+        FromSqliteUser.delete_one(session, filters=[FromSqliteUser.name == "John"])
 
-        assert User.find_one(session, filters=[User.name == "John"]) is None
+        assert (
+            FromSqliteUser.find_one(session, filters=[FromSqliteUser.name == "John"])
+            is None
+        )
 
 
 def test_unique_user_insert_or_ignore(sync_session_manager: SyncSessionManager):
-    user = UniqueUser(name="John", age=20, created_at=datetime.now())
+    user = FromSqliteUniqueUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         user.__create_table__(sync_session_manager.engine)
         user.save(session)
 
-        user2 = UniqueUser(name="Andrew", age=21, created_at=datetime.now())
+        user2 = FromSqliteUniqueUser(name="Andrew", age=21, created_at=datetime.now())
 
         user2.insert_or_ignore(session)
 
-        assert UniqueUser.find_one(session, filters=[UniqueUser.name == "John"]) == user
+        assert (
+            FromSqliteUniqueUser.find_one(
+                session, filters=[FromSqliteUniqueUser.name == "John"]
+            )
+            == user
+        )
 
-        filter_user2 = UniqueUser.find_one(
-            session, filters=[UniqueUser.name == "Andrew"]
+        filter_user2 = FromSqliteUniqueUser.find_one(
+            session, filters=[FromSqliteUniqueUser.name == "Andrew"]
         )
         assert filter_user2 is not None
         assert filter_user2.name == "Andrew"
         assert filter_user2.age == 21
 
-        update_user2 = UniqueUser(name="Andrew", age=22, created_at=datetime.now())
+        update_user2 = FromSqliteUniqueUser(
+            name="Andrew", age=22, created_at=datetime.now()
+        )
         update_user2.insert_or_ignore(session)
 
-        filter_user2 = UniqueUser.find_one(
-            session, filters=[UniqueUser.name == "Andrew"]
+        filter_user2 = FromSqliteUniqueUser.find_one(
+            session, filters=[FromSqliteUniqueUser.name == "Andrew"]
         )
         assert filter_user2 is not None
         assert filter_user2.name == "Andrew"
@@ -158,24 +176,26 @@ def test_unique_user_insert_or_ignore(sync_session_manager: SyncSessionManager):
 
 
 def test_unique_user_insert_or_update(sync_session_manager: SyncSessionManager):
-    user = UniqueUser(name="John", age=20, created_at=datetime.now())
+    user = FromSqliteUniqueUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         user.__create_table__(sync_session_manager.engine)
         user.save(session)
 
-        user1 = UniqueUser(name="John", age=21, created_at=datetime.now())
+        user1 = FromSqliteUniqueUser(name="John", age=21, created_at=datetime.now())
         user1.insert_or_update(session)
 
-        filter_user1 = UniqueUser.find_one(session, filters=[UniqueUser.name == "John"])
+        filter_user1 = FromSqliteUniqueUser.find_one(
+            session, filters=[FromSqliteUniqueUser.name == "John"]
+        )
         assert filter_user1 is not None
         assert filter_user1.age == 21
 
 
 def test_sync_get_except(sync_session_manager: SyncSessionManager):
-    unique_user = UniqueUser(name="John", age=20, created_at=datetime.now())
-    unique_user2 = UniqueUser(name="Jane", age=21, created_at=datetime.now())
-    user = User(name="John", age=20, created_at=datetime.now())
+    unique_user = FromSqliteUniqueUser(name="John", age=20, created_at=datetime.now())
+    unique_user2 = FromSqliteUniqueUser(name="Jane", age=21, created_at=datetime.now())
+    user = FromSqliteUser(name="John", age=20, created_at=datetime.now())
 
     with sync_session_manager.get_session() as session:
         unique_user.__create_table__(sync_session_manager.engine)
@@ -185,10 +205,10 @@ def test_sync_get_except(sync_session_manager: SyncSessionManager):
         user.__create_table__(sync_session_manager.engine)
         user.save(session)
 
-        except_users = UniqueUser.get_except(
+        except_users = FromSqliteUniqueUser.get_except(
             session,
             except_key1="name",
-            cls2=User,
+            cls2=FromSqliteUser,
             except_key2="name",
         )
 
